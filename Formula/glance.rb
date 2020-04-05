6f363fbfe7cabe12532daf73e303a459eac28f56
davidxarnold <darnold@soundofcorduroy.com>
2020-04-05 21:52:07 +0000

    update formula

--- Formula/glance.rb
+++ Formula/glance.rb
@@ -1,8 +1,8 @@
 class Glance < Formula
     desc "A kubectl plugin to view cluster resource allocation and usage."
     homepage "https://github.com/davidxarnold/glance"
-    url "https://gitlab.com/davidxarnold/glance/-/jobs/489448124/artifacts/raw/archive/kubectl-glance-0.0.1.tar.gz?job=build-darwin"
-    sha256 "fdb6d8ea9c301a119a1840c16fc0d4819710594ddcb66954274aa628b9eddeb4"
+    url "https://gitlab.com/davidxarnold/glance/-/jobs/499401216/artifacts/raw/archive/kubectl-glance-0.0.1.tar.gz?job=build-darwin"
+    sha256 "6fe39ca45beb6334dcaed6c27e45d1d94162d4ab05bbb53b5f54386424cf6d5e"
     version "0.0.1"
     
     def install
