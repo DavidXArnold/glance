638ca20324365024b40dee6ffe505b410a682fe8
davidxarnold <darnold@soundofcorduroy.com>
2025-12-27 19:40:26 +0000

    chore: update homebrew formula for v0.1.0

--- Formula/glance.rb
+++ Formula/glance.rb
@@ -2,8 +2,8 @@ class Glance < Formula
     desc "A kubectl plugin to view cluster resource allocation and usage."
     homepage "https://github.com/davidxarnold/glance"
     url "https://gitlab.com/davidxarnold/glance/-/jobs/499407379/artifacts/raw/archive/kubectl-glance-0.0.1.tar.gz?job=build-darwin"
-    sha256 "fd7d0354d88bb00d40b8e19ef0bb3c4329918bc64cfb1b09941dbdf0f3f329e3"
-    version "0.0.1"
+    sha256 "0408a72e01f8aa4532f659c0bc4f6112c5279ca791fd3fee6bf234ba9cc84f99"
+    version "0.1.0"
     
     def install
       bin.install "kubectl-glance"
